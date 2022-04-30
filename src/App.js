d48159a938f5730680de2d5c9ecc09dbbe01419e
Md Arfan Khan <khan188993@gmail.com>
2022-04-30 16:31:34 +0600

    1)React Router v6 Initial Start

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <>
-      <h1>React Learning Practice code Running.</h1>
+      <h1>React Router V6 Practice code Running.</h1>
     </>
   )
 }
