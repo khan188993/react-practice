e803ba8d8581c9d24f87539e12218279d729d7fd
Md Arfan Khan <khan188993@gmail.com>
2022-04-30 16:56:12 +0600

    4)Basic Css inline Style adding

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { Link, NavLink, Route,Routes } from 'react-router-dom';
 
 const App = () => {
+
+  // all style for a tag 
+  const allStyle = {
+    background:'yellow',
+    color:'blue',
+  }
+
   return (
     <>
-      <h1>React Router V6 Practice code Running.</h1><hr />
+      {/* Basic Inline CSS Style Add */}
+      <h1 style={{color:"red",backgroundColor:"yellow"}}>React Router V6 Practice code Running.</h1><hr />
       {/* Navigation Adding */}
       <ul>
         {/* A Tag Will Refresh the page */}
@@ -19,7 +27,7 @@ const App = () => {
       {/* React Router Basic Systems added */}
       <Routes>
           <Route path="/" element="Hello Router Home"></Route>
-          <Route path="about" element={<h1>Hello Router About</h1>}></Route>
+          <Route path="about" element={<h1 style={allStyle}>Hello Router About</h1>}></Route>
           <Route path="/blog" element="Hello Router Blog"></Route>
           {/* this will run component */}
           <Route path="/basic-component" element={<BasicComponent/>}></Route>
