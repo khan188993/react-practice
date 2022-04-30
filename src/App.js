fe44005ec00eec14f2152451d4f98593a23f515e
Md Arfan Khan <khan188993@gmail.com>
2022-04-30 16:44:43 +0600

    2)React Router Basic Usage Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Route,Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-      <h1>React Router V6 Practice code Running.</h1>
+      <h1>React Router V6 Practice code Running.</h1><hr />
+      {/* React Router Basic Systems added */}
+      <Routes>
+          <Route path="/" element="Hello Router Home"></Route>
+          <Route path="about" element={<h1>Hello Router About</h1>}></Route>
+          <Route path="/blog" element="Hello Router Blog"></Route>
+          {/* this will run component */}
+          <Route path="/basic-component" element={<BasicComponent/>}></Route>
+      </Routes>
     </>
+  )
+}
+
+//Extra Basic Component For Routing 
+const BasicComponent = ()=>{
+  return (
+    <h1>Basic Component</h1>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
