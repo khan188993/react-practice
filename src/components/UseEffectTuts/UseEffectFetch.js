441da8c2c2d872b6a1da41544fcf0c64c9d33272
Md Arfan Khan <khan188993@gmail.com>
2022-05-01 22:43:20 +0600

    13)search userData Address

--- src/components/UseEffectTuts/UseEffectFetch.js
+++ src/components/UseEffectTuts/UseEffectFetch.js
@@ -3,25 +3,42 @@ import './UseEffectFetch.css'
 
 const UseEffectFetch = () => {
     const [users,setUsers] = useState([]);
+    const [loading,setLoading] =useState(true);
 
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
         .then((res)=>res.json())
         .then((data)=>{
             // console.log(data);
+            setLoading(false)
             setUsers(data) // after calling this 15 number line will rerender
         })
     },[])
 
     console.log(users); //data fetch krte object caining use krte hobe must 
 
+    //handle Search function for search data 
+    const handleSeach = (e)=>{
+        e.preventDefault();
+        let searchCity = e.target.search.value;
+        if(searchCity){
+            let searchUsers = users.filter((user)=> user.address.street.toLowerCase() === searchCity.toLowerCase())
+            setUsers(searchUsers);
+            console.log('search fun',searchCity,'searchUsers',searchUsers);
+        }
+    }
+
 
     return (
         <div>
             <h1>UseEffect fetch Data </h1><hr></hr>
+            <form action="" onSubmit={handleSeach}>
+                Search by Id: <input type="text" name="search"/>
+                <input type="submit" value="submit" />
+            </form>
             <div className="data-container">
                 {
-                    users.length > 0 ?(
+                    users.length > 0 && !loading ?(
                         users.map((user)=>{
                             console.log(user);
                             return (
