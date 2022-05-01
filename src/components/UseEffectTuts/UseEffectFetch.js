ec1349cca66981f5dd47659cf03ace13022a5f72
Md Arfan Khan <khan188993@gmail.com>
2022-05-01 22:20:08 +0600

    11)useEffect Fetch Data Loaded

--- src/components/UseEffectTuts/UseEffectFetch.js
+++ src/components/UseEffectTuts/UseEffectFetch.js
@@ -1,23 +1,38 @@
 import React,{useState,useEffect} from 'react'
+import './UseEffectFetch.css'
 
 const UseEffectFetch = () => {
-    const [user,setUser] = useState([]);
+    const [users,setUsers] = useState([]);
 
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
         .then((res)=>res.json())
         .then((data)=>{
             // console.log(data);
-            setUser(data) // after calling this 15 number line will rerender
+            setUsers(data) // after calling this 15 number line will rerender
         })
     },[])
 
-    console.log(user[0]?.name); //data fetch krte object caining use krte hobe must 
-    
+    console.log(users); //data fetch krte object caining use krte hobe must 
+
 
     return (
         <div>
             <h1>UseEffect fetch Data </h1><hr></hr>
+            <div className="data-container">
+                {
+                    users.length > 0 && users.map((user)=>{
+                        console.log(user);
+                        return (
+                            <div className="single-data" key={user.id}>
+                                <h1>Name : {user.name}</h1>
+                                <h3>Phone : {user.phone}</h3>
+                                <h5>Address:{user.address.street}</h5>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
