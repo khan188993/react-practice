0e65fd7acedab49bcf304d9e922804a7a43612f6
Md Arfan Khan <khan188993@gmail.com>
2022-05-01 13:24:36 +0600

    5)Advance css Adding Way added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
+
+//App.css priority first 
+// import './Index.css'
+import './App.css'
+import ClassBased from './ClassBased.module.css'
 import { Link, NavLink, Route,Routes } from 'react-router-dom';
 
 const App = () => {
 
-  // all style for a tag 
-  const allStyle = {
-    background:'yellow',
-    color:'blue',
-  }
-
   return (
     <>
       {/* Basic Inline CSS Style Add */}
-      <h1 style={{color:"red",backgroundColor:"yellow"}}>React Router V6 Practice code Running.</h1><hr />
+      <h1>React Router V6 Practice code Running.</h1><hr />
       {/* Navigation Adding */}
       <ul>
         {/* A Tag Will Refresh the page */}
@@ -20,6 +19,7 @@ const App = () => {
 
         {/* This will not Refresh the page */}
         <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink  to="/about">About</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/blog">Blog</NavLink></li>
         <li><Link to="/basic-component">Basic Component</Link></li>
@@ -27,11 +27,15 @@ const App = () => {
       {/* React Router Basic Systems added */}
       <Routes>
           <Route path="/" element="Hello Router Home"></Route>
-          <Route path="about" element={<h1 style={allStyle}>Hello Router About</h1>}></Route>
+          <Route path="about" element={<h1>Hello Router About</h1>}></Route>
           <Route path="/blog" element="Hello Router Blog"></Route>
           {/* this will run component */}
           <Route path="/basic-component" element={<BasicComponent/>}></Route>
       </Routes>
+
+      {/* Css Adding Style  */}
+      <CssAddingComponent/>
+
     </>
   )
 }
@@ -40,6 +44,47 @@ const App = () => {
 const BasicComponent = ()=>{
   return (
     <h1>Basic Component</h1>
+  )
+}
+
+
+const CssAddingComponent = ()=>{
+    // all style for a tag 
+    const allStyle = {
+      background:'yellow',
+      color:'blue',
+    }
+  
+    const myClass ="myclass-design";
+    const bgAdding = "./logo192.png";
+    const number = '192';
+    console.log(`url("logo"${number}".png")`,`url("${["logo",number,".png"].join("")}")`);
+    console.log(ClassBased)
+
+
+  return (
+    <>
+       {/* CSS Adding Tricks */}
+       <h1>CSS Adding Tricks:</h1><hr></hr>
+      <ul>
+        <li style={{color:"red",backgroundColor:"yellow"}}>Inline CSS Direct</li>
+        <li style={allStyle}>Inline Css variable</li>
+        <li className={`text-orange ${myClass}`}>Class based css with dynamic variable</li>
+        <li className="myclass-design">Class Css simple String</li>
+        <li className={['text-orange',myClass].join(" ")}>Join Based Desing</li>
+        <li className={true?'text-orange':'myclass-design'}>Condition Based Class Desing</li>
+        <li style={true?{background:'red'}:{color:'red'}}>Condition Based inline Style</li>
+        {/* can't use this variant ,background Image used*/}
+        <li style={true && {backgroundImage:'url("logo192.png")',backgroundSize:'contain',color:'green'}}>direct bg adding with inline style </li>
+        <li style={{backgroundImage:`url(${bgAdding})`,backgroundSize:'contain',color:'green'}}>bg adding by variable with inline style </li>
+        {/* this will not work */}
+        {/* <li style={{backgroundImage:`url("logo"${number}".png")`,backgroundSize:'cover',color:'red'}}>Trick </li> */}
+        {/* console e age dekhe nibo */}
+        <li style={{backgroundImage:`url("${["logo",number,".png"].join("")}")`,backgroundSize:'contain'}}>bg adding with string + dynamic variable </li>
+        {/* module based css priority first */}
+        <li className={`myclass-design ${ClassBased.new} `}>Module Based CSS Adding </li>
+      </ul>
+    </>
   )
 }
 
---
+++ sticky-notes.js
@@ -0,0 +1,19 @@
+/**
+ * css inline basic uses 
+ * css object type style use 
+ * css background use 
+ * css file type style use,
+ * css condition type use by true false,
+ * ------------------------------
+ * css route active adding by class and inline style,
+ * params send and catch by another component,
+ * searchParam(?data="my data send") send and catch,
+ * 404 route add, 
+ * multiple route params adding,
+ * dashboard login log out,
+ * in true dashboard send false navigate to login , logout button to logout with msg data,
+ * private route adding,
+ * navigate with state data, useLocation,
+ * difference with navigate(), Navigate,
+ * useLocation full object value 
+ */
