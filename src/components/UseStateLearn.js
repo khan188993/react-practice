9e178f646dc80e956772c7962d9bfc4e56a974c2
Md Arfan Khan <khan188993@gmail.com>
2022-05-01 19:53:11 +0600

    3)object type state management handle

--- src/components/UseStateLearn.js
+++ src/components/UseStateLearn.js
@@ -1,24 +1,70 @@
-import React from "react";
+import React,{useState} from "react";
 
 const UseStateLearn = () => {
+    const [number,setNumber] = useState(0);
+    const [name,setName] =useState("");
+    const [email,setEmail] =useState("");
+    const [user,setUser] = useState({});
 
-    //React State Manupulate kora jai na useState management chara,
-    let data = "Arfan khan is a good boy";
+    const changeNumber = ()=>{
+        // setNumber(number+1);
+        setNumber((prev)=>prev+1);
+    }
+
+    const submitHandle = (e)=>{
+        e.preventDefault();
+        console.log('name',name,'email',email);
+        if(name && email){
+            setUser({name:name,email:email});
+        }else{
+            alert('please set userInput')
+        }
+        
+    }
+
+    //?React State Manupulate kora jai na useState management chara,
+    /* let data = "Arfan khan is a good boy";
     let count = 0;
     const changeData = ()=>{
         data = "Bad Boy";
         count = count+1;
         console.log(data,'count',count);
     }
-    console.log(data);
+    console.log(data); */
 
     return (
         <div>
             <h1>UseState Learn</h1>
-            <p>{data} and {count}</p>
-            <button onClick={changeData}>Click to change</button>
+            {/* <p>{data} and {count}</p> */}
+            <h4>Number : {number}</h4>
+            <button onClick={changeNumber}>Click to change</button>
+            <button onClick={()=>{changeNumber()}}>Click to change 2nd Method</button>
+            <button onClick={()=>setNumber(number+1)}>Click to change 3rd Method</button>
+            <hr />
+
+            {/* Objecct Handle data */}
+            {Object.keys(user).length > 0 && (<h4>ObjectData Handle: Name = {user.name} || email = {user.email}</h4>) }
+            <form action="" onSubmit={submitHandle}>
+                Name : <input type="text" onBlur={(e)=>setName(e.target.value)}/>
+                Email : <input type="email" onBlur={(e)=>setEmail(e.target.value)} />
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
 
 export default UseStateLearn;
+
+/* 
+singleData useState management,
+---------------------------------------
+1)simple way to call setNumber((prev)=>prev),
+2)setNumber(directChange);setNumber(number+1);
+3)onClick{chagneNumber} / onClick={()=>changeNumber()} / onClick={()=>setNumber(directChange)}
+build in gulo arrow function e dite hobe,
+--------------------------------------
+object Data type useState management:
+
+
+
+*/
